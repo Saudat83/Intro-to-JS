f2b8bb3ba73fda05678ae97bcc85a37c2d3d261b
Saudat Sanni <105182062+Saudat83@users.noreply.github.com>
2022-06-22 10:30:01 -0700

    first js

---
+++ Intro.js
@@ -0,0 +1,4 @@
+let myName = "Saudat Sanni";
+let myHeight = 1.65;
+let myCountry = "Nigeria";
+document.write("My name is "+myName +"<br>"+" "+"My height is "+myHeight +"<br>"+" "+"The name of my Country is "+myCountry)
